9f6219dbc250917bee0cdcb84577f440e9099c15
SEGARS <Vim_Branet@mail.ru>
2025-06-04 13:49:45 +0500

    работа дома

---
+++ Project2/main.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+/*1. 
+Создай класс Point 
+Это будет точка в пространстве 
+У нее внутри будет две переменные x и y, 
+это координаты нашей точки 
+Создай вектор элементов этих точек 
+В вектор надо добавить 3 точки 
+Координаты для них надо запросить у пользователя 
+Далее пробегись по вектору и определи какая из точек находится выше всех по y, 
+и выведи ее координаты на консоль
+*/
+
+class Point
+{
+public:
+	int x;
+	int y;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	std::vector<Point> point;
+
+	int cor = 3;
+
+	std::cout << "Ввести кординаты X - Y:" << std::endl;
+
+	for (; cor > 0; --cor)
+	{
+		Point zapPoint;
+		std::cin >> zapPoint.x;
+		std::cin >> zapPoint.y;
+
+		point.push_back(zapPoint);
+	}
+
+	for (int i = 0; i < point.size(); i++)
+	{
+		std::cout << "Кордината X: " << point[i].x << " Кордината Y: " << point[i].y << std::endl;
+	}
+
+	std::cout << "Находим точку У которая находится выше всех." << std::endl;
+
+	int maxX = 0;
+	int maxY = 0;
+
+	for (int i = 0; i < point.size(); i++)
+	{
+		if (point[i].y > maxY && point[i].x > 0)
+		{
+			maxY = point[i].y;
+			maxX = point[i].x;
+		}
+	}
+
+	std::cout << "Выще всех точка кординат по оси Y - : " << "Кордината X: " << maxX << " Кордината Y: " << maxY << std::endl;
+
+	return 0;
+}
